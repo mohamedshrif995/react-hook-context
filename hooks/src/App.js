476cecabeaf83e223ac6dd8da3ddccf2832934a7
M Sherif <msherif@8fits-MacBook-Pro-2.local>
2019-11-17 01:59:50 +0200

    add app js

---
+++ hooks/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import SongList from './component/SongList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <SongList />
+    </div>
+  );
+}
+
+export default App;
